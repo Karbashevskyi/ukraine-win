2338cbf1d9fbaf7ed5a14c3ac68bd54662ed98e7
Ivan Karbashevskyi <ivankarbashevskyi@gmail.com>
2022-03-03 00:25:18 +0100

    Add Proxy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ import {SourceEnum} from '@app/common/enums/source.enum';
 
 export const environment = {
   host: {
-    [SourceEnum.GITHUB]: SourceEnum.GITHUB
+    [SourceEnum.GITHUB]: 'https://raw.githubusercontent.com/Karbashevskyi/ukraine-win/main'
   },
   production: false
 };
